957a860a09cf51b14c4c313dcc414780ecfe4c68
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2019-11-11 22:50:20 +0100

    updates

--- sketch.js
+++ sketch.js
@@ -1,11 +1,51 @@
-function preload(){
-  // put preload code here
+var myMap;
+var canvas;
+
+var position;
+
+var mappa = new Mappa('MapboxGL',
+'pk.eyJ1Ijoic25vd2RpbjE2IiwiYSI6ImNrMm4yeDF1NTBtZWEzaW41bndlcDZtazQifQ.qaidFM19qHeTtXx0ClItmg');
+
+
+var options = {
+  lat: 0,
+  lng: 0,
+  zoom: 10,
+  studio: true,
+  style: "mapbox://styles/snowdin16/ck2uwvo9007kq1co6mephjjhg"
+}
+
+
+var coordinates;
+
+
+function preload() {
+  position = getCurrentPosition();
+  console.log(position); //i'm here!!
+  pin = loadImage("./assets/pin.png"); //pointer
+
+
+  cursor(CROSS)
 }
 
 function setup() {
-  // put setup code here
+  options.lat = position.latitude;
+  options.lng = position.longitude;
+
+  canvas = createCanvas(windowWidth, windowHeight);
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas); //to make the map visible
+
+
 }
 
 function draw() {
-  // put drawing code here
+  clear();
+  coordinates = myMap.latLngToPixel(position.latitude, position.longitude);
+  // ellipse(coordinates.x, coordinates.y, 20);
+  // noStroke()
+  // fill("red")
+  image(pin, coordinates.x, coordinates.y, 120, 110)
+
+
 }
